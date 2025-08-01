73047ca32dea1d8958901ecf86e9218bebda80d1
Balakrishna_T <tbalakrishna2005@gmail.com>
2025-08-01 15:20:59 +0530

    Added Effects

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ function playGame(userChoice) {
   const computerChoice = choices[Math.floor(Math.random() * 3)];
 
   document.getElementById('user-choice').textContent = userChoice;
+  setTimeout(()=>{
+    document.getElementById('user-choice').textContent = ""
+    },1000)
   document.getElementById('computer-choice').textContent = computerChoice;
+  setTimeout(()=>{
+    document.getElementById('computer-choice').textContent = ""
+    },1000)
 
   const result = getResult(userChoice, computerChoice);
   document.getElementById('outcome').textContent = 'Result: ' + result;
+  setTimeout(()=>{
+    document.getElementById('outcome').textContent = 'Result: '
+    },1000)
+  
 }
 
 function getResult(user, computer) {
